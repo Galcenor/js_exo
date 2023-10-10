7ad3c29c9c43eb36f44b402c2707b4468e927dd3
Galcenor <lemairelenny02@gmail.com>
2023-10-10 15:03:01 +0200

    Partie 2

--- Partie2/Exo 7/index.php
+++ Partie2/Exo 7/index.php
@@ -1,24 +1,100 @@
-<html>
+
+
+<!DOCTYPE html>
+ 
 <head>
-<title> Password Matching Validation </title>
+    <title>Double Mot de Passe</title>
+    <style>
+        body {
+            margin: 0;
+            padding: 0;
+            background-color: rgb(50, 57, 78);
+            display: flex;
+            justify-content: center;
+            align-items: center;
+        }
+ 
+        .main {
+ 
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+            background-color: rgb(34, 34, 34);
+            height: 400px;
+            width: 300px;
+            margin-top: 15%;
+            border-radius: 10px;
+            box-shadow: 2px 4px 6px rgb(0, 0, 0);
+        }
+ 
+        .pass {
+            display: flex;
+            flex-direction: column;
+        }
+ 
+        .image h2 {
+            color: rgb(2, 149, 27);
+            font-size: 30px;
+            font-family: sans-serif;
+            margin-bottom: 50px;
+        }
+ 
+        .username input,
+        .pass input {
+            font-family: sans-serif;
+            margin-bottom: 20px;
+            height: 30px;
+            border-radius: 100px;
+            border: none;
+            text-align: center;
+            outline: none;
+        }
+ 
+        .submit_btn {
+            height: 30px;
+            width: 80px;
+            border-radius: 100px;
+            border: none;
+            outline: none;
+            background-color: rgb(0, 179, 95);
+            margin-top: 15px;
+        }
+ 
+        .submit_btn:hover {
+            background-color: rgba(0, 179, 95, 0.596);
+            color: rgb(14, 14, 14);
+            cursor: pointer;
+        }
+    </style>
 </head>
+ 
 <body>
-<form>
-<h1 style="color:green">Exercice 7</h1>
-<!-- Entrer le Mot de Passe -->
-<td> Entrez le Mot de Passe </td>
-<input type = "password" name = "pswd1"> <br><br>
-
-<!-- Confirmer -->
-<td> Retapez le Mot de Passe  </td>
-<input type = "password" name = "pswd2"> <br><br>
-
-<!-- Click to validate confirm password -->
-<button type = "submit" onclick="matchPassword()">Submit</button>
-
-<!-- Click to reset fields -->
-<button type = "reset" value = "Reset" >Reset</button>
-</form>
+    <div class="main">
+        <div class="image">
+            <h2>Exercice 7</h2>
+        </div>
+        <div class="pass">
+            <input id="pass"
+                   type="password"
+                   name="pass"
+                   placeholder="Entrez le Mot de Passe"
+                   required"">
+            <input id="confirm_pass"
+                   type="password"
+                   name="confirm_pass"
+                   placeholder="Confirmez le Mot de Passe" required
+                   onkeyup="validate_password()">
+        </div>
+        <span id="wrong_pass_alert"></span>
+        <div class="buttons">
+            <button id="create"
+                    class="submit_btn"
+                    onclick="wrong_pass_alert()">
+                Envoyez
+            </button>
+        </div>
+    </div>
+    <script src="./script.js"></script>
 </body>
-<script src="./script.js"></script>
 </html>
--- Partie2/Exo 7/script.js
+++ Partie2/Exo 7/script.js
@@ -1,10 +1,32 @@
-function matchPassword() {
-    var pw1 = document.getElementById("pswd1");
-    var pw2 = document.getElementById("pswd2");
-    if(pw1 !== pw2)
-    {	
-        alert("Passwords did not match");
+function validate_password() {
+ 
+    var pass = document.getElementById('pass').value;
+    var confirm_pass = document.getElementById('confirm_pass').value;
+    // Demande si les deux mot de passes sont les mêmes
+    if (pass != confirm_pass) {
+        // Met une couleur et indique que les deux mot de passe ne sont pas identique
+        document.getElementById('wrong_pass_alert').style.color = 'red';
+        document.getElementById('wrong_pass_alert').innerHTML
+            = '☒ Veuillez à utiliser le même mot de passe';
+        // Permet de ne pas pouvoir cliquer sur le bouton
+        document.getElementById('create').disabled = true;
+        document.getElementById('create').style.opacity = (0.4);
     } else {
-        alert("Password created successfully");
+        // Met une couleur et indique que les deux mot de passe sont identique
+        document.getElementById('wrong_pass_alert').style.color = 'green';
+        document.getElementById('wrong_pass_alert').innerHTML =
+            '🗹 Les deux mot de passe sont identiques';
+        // Permet de pouvoir cliquer sur le bouton
+        document.getElementById('create').disabled = false;
+        document.getElementById('create').style.opacity = (1);
     }
-  }
+}
+
+function wrong_pass_alert() {
+    if (document.getElementById('pass').value != "" &&
+        document.getElementById('confirm_pass').value != "") {
+        alert("Your response is submitted");
+    } else {
+        alert("Please fill all the fields");
+    }
+}
